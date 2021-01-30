e7917eee3f7fe2e348f6d753f702e1e797f645e0
Paruthi <Paruthidotexe@gmail.com>
2021-01-30 10:43:54 +0530

    created new scene for testing cameras

---
+++ GGJ20213D/Assets/LostAndFound/Scripts/CamTest.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CamTest : MonoBehaviour
+{
+    public Transform playerTransform;
+    public Transform cameraTransform;
+    Vector3 playerPos = Vector3.zero;
+    Vector3 camPos = Vector3.zero;
+    Vector3 offset = Vector3.zero;
+
+    void Start()
+    {
+        camPos = cameraTransform.position;
+        playerPos = playerTransform.position;
+        offset = camPos - playerPos;
+    }
+
+    void Update()
+    {
+
+    }
+
+    private void LateUpdate()
+    {
+        cameraTransform.position = playerTransform.position + offset;
+    }
+}
+
