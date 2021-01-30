dee03fdd4679054a6fd37570ec1e83c0fd0c5583
Paruthi <Paruthidotexe@gmail.com>
2021-01-31 01:34:11 +0530

    Merging

--- GGJ20213D/Assets/LostAndFound/Scripts/PuppyController.cs
+++ GGJ20213D/Assets/LostAndFound/Scripts/PuppyController.cs
@@ -13,7 +13,7 @@ public class PuppyController : MonoBehaviour
     [SerializeField] private float rotationSpeed = 4.0f;
 
     private CharacterController controller;
-    private Vector3 playerVelocity;
+    private Vector3 playerVelocity = Vector3.zero;
     private bool groundedPlayer;
 
     private Transform cameraMainTransform;
@@ -21,6 +21,8 @@ public class PuppyController : MonoBehaviour
     public PlayerData playerData;
 
     private AudioManager audioManager;
+
+    private float speedBoost = 1f;
 
     private void OnEnable()
     {
@@ -66,7 +68,16 @@ public class PuppyController : MonoBehaviour
         move = cameraMainTransform.forward * move.z + cameraMainTransform.right * move.x;
         move.y = 0;
 
-        controller.Move(move * Time.deltaTime * playerSpeed);
+        controller.Move(move * Time.deltaTime * playerSpeed * speedBoost);
+
+        if(speedBoost > 1f)
+        {
+            speedBoost -= 0.001f;
+        }
+        else
+        {
+            speedBoost = 1f;
+        }
 
         //if (move != Vector3.zero)
         //{
@@ -89,6 +100,25 @@ public class PuppyController : MonoBehaviour
             Quaternion rotation = Quaternion.Euler(0, targetAngle, 0);
             transform.rotation = Quaternion.Lerp(transform.rotation, rotation, rotationSpeed * Time.deltaTime);
         }
+    }
+
+    //Detect collisions between the GameObjects with Colliders attached
+    void OnControllerColliderHit(ControllerColliderHit hit)
+    {
+        Debug.Log("OnCollisionEnter");
+
+        //Check for a match with the specified name on any GameObject that collides with your GameObject
+        if (hit.gameObject.tag == "SpeedMushroom")
+        {
+            SpeedMushroomScript mushroom = hit.gameObject.GetComponent<SpeedMushroomScript>();
+            if (mushroom.CanEat())
+            {
+                audioManager.Play("interaction", hit.gameObject.transform.position);
+                mushroom.Eat();
+                speedBoost = 5;
+            }
+        }
+
     }
 
 }
